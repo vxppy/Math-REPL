25fa2233954ea625a1a737418d94934101f90187
Venger-King <earthingout01@gmail.com>
2023-03-22 09:19:52 +0530

    bugfix #2 fixed a possible memory leak

--- input.js
+++ input.js
@@ -2,6 +2,7 @@
  * @typedef { 'ascii' | 'base64' | 'hex' | 'ucs2' | 'ucs-2' | 'utf16le' | 'utf-16le' | 'utf8' | 'utf-8' | 'binary' | 'latin1' } Encoding
  */
 
+
 /**
  * @param { string } [text] 
  * @param { Encoding } encoding
@@ -18,37 +19,16 @@ const input = async (text, encoding = 'utf-8') => {
     return new Promise((resolve) => {
         process.stdin.resume()
 
-        process.stdin.on('data', function (data) {
+        let listen = function (data) {
             process.stdin.pause()
             process.stdin.setEncoding(_enc)
+            process.stdin.removeListener('data', listen)
             resolve(encoding = 'utf-8' ? data.trim() : data)
-        })
-
-    })
-}
-
-/**
- * @param { string } [text]
- * @param { (data: string) => void } callback
- * @param { Encoding } encoding
- */
-const inputSync = (text, callback, encoding = 'utf-8') => {
-
-    let _enc = process.stdin._readableState.encoding || process.stdin._readableState.defaultEncoding
-    process.stdin.setEncoding(encoding)
-
-    if (text) process.stdout.write(text)
-
-
-    process.stdin.resume()
-
-    process.stdin.on('data', function (data) {
-        process.stdin.pause()
-        process.stdin.setEncoding(_enc)
-        callback(encoding = 'utf-8' ? data.trim() : data)
+        }
+        process.stdin.on('data', listen)
     })
 }
 
 module.exports = {
-    input, inputSync
+    input
 }
